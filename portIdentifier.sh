d5500f7fb7bda912398efc45585f1201c1c8f406
XesKo <144061131+X5K0@users.noreply.github.com>
2024-02-19 14:20:27 -0500

    Add files via upload

---
+++ portIdentifier.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Description: Alternativa para la enumeración de puertos usando descriptores de archivos.
+# Author: Francisco Senén Gómez Méndez
+
+function ctrl_c(){
+	echo -e "\n\n[!] Saliendo...\n"
+	tput cnorm; exit 1
+}
+
+# Ctrl + C
+trap ctrl_c SIGINT
+
+declare -a ports=( $( seq 1 65535 ) )
+
+function checkPort(){
+	(exec 3<> /dev/tcp/$1/$2) 2>/dev/null
+
+	if [ $? -eq 0 ]; then
+		echo "[+] Host $1 - Port $2 (OPEN)"
+	fi
+
+	exec 3<&-
+	exec 3>&-
+}
+
+tput civis # Ocultar el cursor
+
+if [ $1 ]; then
+	for port in ${ports[@]}; do
+		checkPort $1 $port &
+	done
+else
+	echo -e "\n[!] Uso: $0 <ip-address>\n"
+fi
+
+wait
+
+tput cnorm
